ac6e28a64562eebef66179cd9194073a30987538
Tomasz Dobrowolski <chrumck@gmail.com>
2016-12-20 22:04:58 -0800

    clean up scratchpad

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/chrumck/CtciExercises/ScratchPad"
 
 func main() {
-	//    fmt.Println(ScratchPad.NewtonSqrt(2))
-	//    fmt.Println(math.Sqrt(2))
+	//fmt.Println(ScratchPad.NewtonSqrt(2))
+	//fmt.Println(math.Sqrt(2))
 
 	ScratchPad.Crawl("http://golang.org/", 10, ScratchPad.MakeFakeFetcher())
 }
