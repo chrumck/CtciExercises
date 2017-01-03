60dc19a1e9dbaf7476680e591afeb73f84b7a50d
Tomasz Dobrowolski <chrumck@gmail.com>
2017-01-02 18:43:09 -0800

    implemented QuickSort

--- sorting/QuickSort.go
+++ sorting/QuickSort.go
@@ -6,5 +6,33 @@ import (
 
 // QuickSort sorts given data using QuickSort algorithm
 func QuickSort(data sort.Interface) {
+	// TODO: add shuffle
+	quickSort(data, 0, data.Len()-1)
+}
+
+func quickSort(data sort.Interface, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+	j := partition(data, lo, hi)
+	quickSort(data, lo, j-1)
+	quickSort(data, j+1, hi)
+}
+
+func partition(data sort.Interface, lo, hi int) (j int) {
+	i, j := lo+1, hi
+	for {
+		for i < hi && data.Less(i, lo) {
+			i++
+		}
+		for j > lo && data.Less(lo, j) {
+			j--
+		}
+		if i >= j {
+			break
+		}
+		data.Swap(i, j)
+	}
+	data.Swap(lo, j)
 	return
 }
