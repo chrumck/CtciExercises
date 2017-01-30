35165d2f52413bd73be23ad4860983a092c7a19d
Tomasz Dobrowolski <chrumck@gmail.com>
2017-01-29 17:21:28 -0800

    added TripleStep

---
+++ recursionAndDynamicProgramming/TripleStep.go
@@ -0,0 +1,24 @@
+package recursionAndDynamicProgramming
+
+// TripleStep calculates in how many ways a child can hop the stairs if it takes 1, 2, or 3 steps.
+func TripleStep(n int) (ways int) {
+
+	var tripleStep func(stepCountSoFar int)
+	tripleStep = func(stepCountSoFar int) {
+		if stepCountSoFar == n {
+			ways++
+			return
+		}
+
+		const maxSteps = 3
+		for i := 1; i <= maxSteps; i++ {
+			if i <= n-stepCountSoFar {
+				tripleStep(stepCountSoFar + i)
+			}
+		}
+		return
+	}
+
+	tripleStep(0)
+	return
+}
---
+++ recursionAndDynamicProgramming/TripleStep_test.go
@@ -0,0 +1,80 @@
+package recursionAndDynamicProgramming
+
+import (
+	"testing"
+)
+
+func TestTripleStep_WithOneStep_ReturnsCorrectResult(test *testing.T){
+
+	// Arrange
+	sampleTotalSteps := 1
+	expectedWays := 1
+
+	// Act
+	result := TripleStep(sampleTotalSteps)
+
+	// Assert
+	if result != expectedWays {
+		test.Errorf("The result should be %v, but is %v.", expectedWays, result)
+	}
+}
+
+func TestTripleStep_WithTwoSteps_ReturnsCorrectResult(test *testing.T){
+
+	// Arrange
+	sampleTotalSteps := 2
+	expectedWays := 2
+
+	// Act
+	result := TripleStep(sampleTotalSteps)
+
+	// Assert
+	if result != expectedWays {
+		test.Errorf("The result should be %v, but is %v.", expectedWays, result)
+	}
+}
+
+func TestTripleStep_WithThreeSteps_ReturnsCorrectResult(test *testing.T){
+
+	// Arrange
+	sampleTotalSteps := 3
+	expectedWays := 4
+
+	// Act
+	result := TripleStep(sampleTotalSteps)
+
+	// Assert
+	if result != expectedWays {
+		test.Errorf("The result should be %v, but is %v.", expectedWays, result)
+	}
+}
+
+func TestTripleStep_WithFourSteps_ReturnsCorrectResult(test *testing.T){
+
+	// Arrange
+	sampleTotalSteps := 4
+	expectedWays := 7
+
+	// Act
+	result := TripleStep(sampleTotalSteps)
+
+	// Assert
+	if result != expectedWays {
+		test.Errorf("The result should be %v, but is %v.", expectedWays, result)
+	}
+}
+
+func TestTripleStep_WithFiveSteps_ReturnsCorrectResult(test *testing.T){
+
+	// Arrange
+	sampleTotalSteps := 5
+	expectedWays := 13
+
+	// Act
+	result := TripleStep(sampleTotalSteps)
+
+	// Assert
+	if result != expectedWays {
+		test.Errorf("The result should be %v, but is %v.", expectedWays, result)
+	}
+}
